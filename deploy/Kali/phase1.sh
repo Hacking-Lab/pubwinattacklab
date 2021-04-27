01a9cd89153d58f0dc57d72583fbbeb44957f824
Ivan Buetler <ivan.buetler@compass-security.com>
2021-04-27 08:58:28 +0200

    update

--- deploy/Kali/phase1.sh
+++ deploy/Kali/phase1.sh
@@ -5,12 +5,21 @@ cd $myhome
 source $myhome/setup.env
 env > $myhome/allenv.env
 
-# Testing for ENV variables
-echo "${admin_username}" >> $myhome/phase1.env
-echo "${hostname}" >> $myhome/phase1.env
-echo "${storage_account_name}" >> $myhome/phase1.env
-echo "${storage_container_name}" >> $myhome/phase1.env
-echo "${storage_account_sas}" >> $myhome/phase1.env
-echo "${rg_lab_id}" >> $myhome/phase1.env
+# change local hosts file
+sed -i "s/kali/${hostname}/" /etc/hosts
 
+# Updating System
+echo "`date`: starting system update" > /tmp/phase1.log
+export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_PRIORITY=critical
+wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
+apt-get update -yq
+echo "`date`: starting system upgrade" >> /tmp/phase1.log
+apt-get upgrade -yq
+echo "`date`: finished system upgrade" >> /tmp/phase1.log
+
+# Running Kali Tool Installation
+echo "`date`: starting tool installation" >> /tmp/phase1.log
+./phase2.sh
+echo "`date`: finished tool installation" >> /tmp/phase1.log
 
