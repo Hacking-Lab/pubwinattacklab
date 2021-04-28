951e30fdd0cbf369fb3df332b56e60bbb27f534d
Ivan Buetler <ivan.buetler@compass-security.com>
2021-04-28 12:56:34 +0200

    update

--- deploy/Kali/phase1.sh
+++ deploy/Kali/phase1.sh
@@ -6,7 +6,7 @@ source $myhome/setup.env
 env > $myhome/allenv.env
 
 echo "`date`: launching phase2.sh in the background - system upgrade" >> $myhome/phase1.log
-nohup ./phase2.sh &
+screen -d -m -t phase2 ./phase2.sh &
 sleep 3
 echo "`date`: end of script phase1.sh" >> $myhome/phase1.log
 echo "`date`: process list" >> $myhome/phase1.log
